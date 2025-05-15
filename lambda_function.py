ff689a51d335913cb244269b5fdebbb83fe144f9
devang883020 <143190049+devang883020@users.noreply.github.com>
2025-05-15 18:57:39 +0530

    lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from hello from devang welcome to my website!')
+        'body': json.dumps('Hello from hello from devang welcome to my website! and please take me in a company')
     }
